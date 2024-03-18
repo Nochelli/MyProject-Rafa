0bdd7b6e7bfb66445fbe6546ccbcc6e1de0dc411
Jeferson Nochelli <jefersonnochelli@hotmail.com>
2024-03-18 15:53:40 -0300

    a
    a

--- script.js
+++ script.js
@@ -11,46 +11,46 @@
 
 // carrosel
 document.addEventListener("DOMContentLoaded", function() {
-  const carousel = document.querySelector('.carousel');
-  const carouselItems = document.querySelectorAll('.carousel-item');
-  const totalItems = carouselItems.length;
-  let currentIndex = 0;
-  let startX = 0;
-
-  function moveToSlide(index) {
-      currentIndex = index;
-      const translateValue = -index * 100 + '%';
-      carousel.style.transform = `translateX(${translateValue})`;
-  }
-
-  function nextSlide() {
-      currentIndex = (currentIndex + 1) % totalItems;
-      moveToSlide(currentIndex);
-  }
-
-  function prevSlide() {
-      currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-      moveToSlide(currentIndex);
-  }
-
-  function startTouch(e) {
-      startX = e.touches[0].clientX;
-  }
-
-  function endTouch(e) {
-      const endX = e.changedTouches[0].clientX;
-      const difference = startX - endX;
-      if (difference > 50) {
-          nextSlide();
-      } else if (difference < -50) {
-          prevSlide();
-      }
-  }
-
-  setInterval(nextSlide, 3000);
-
-  document.querySelector('.prev').addEventListener('click', prevSlide);
-  document.querySelector('.next').addEventListener('click', nextSlide);
-  carousel.addEventListener('touchstart', startTouch);
-  carousel.addEventListener('touchend', endTouch);
-});
+    const carousel = document.querySelector('.carousel');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const totalItems = carouselItems.length;
+    let currentIndex = 0;
+    let startX = 0;
+  
+    function moveToSlide(index) {
+        currentIndex = index;
+        const translateValue = -index * 100 + '%';
+        carousel.style.transform = `translateX(${translateValue})`;
+    }
+  
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % totalItems;
+        moveToSlide(currentIndex);
+    }
+  
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+        moveToSlide(currentIndex);
+    }
+  
+    function startTouch(e) {
+        startX = e.touches[0].clientX;
+    }
+  
+    function endTouch(e) {
+        const endX = e.changedTouches[0].clientX;
+        const difference = startX - endX;
+        if (difference > 50) {
+            nextSlide();
+        } else if (difference < -50) {
+            prevSlide();
+        }
+    }
+  
+    setInterval(nextSlide, 3000);
+  
+    document.querySelector('.prev').addEventListener('click', prevSlide);
+    document.querySelector('.next').addEventListener('click', nextSlide);
+    carousel.addEventListener('touchstart', startTouch);
+    carousel.addEventListener('touchend', endTouch);
+  });
